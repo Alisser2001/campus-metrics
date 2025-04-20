8509ac261a7f59602b850fdb40509d43a6ae1002
Alisser2001 <estiven.carmona@udea.edu.co>
2025-04-20 15:26:04 -0500

    Creacion de chatbot

--- app/components/chatbot/ChatbotHead.tsx
+++ app/components/chatbot/ChatbotHead.tsx
@@ -10,7 +10,7 @@ interface Props {
 export const ChatbotHead: FC<Props> = ({ isOpen, setIsOpen }) => {
     return (
         <div className="w-full flex flex-row justify-between items-center h-[60px] bg-[#035f2e] text-xl px-5 font-bold text-white">
-            <h1>Chat Assistant</h1>
+            <h1>Asistente Virtual</h1>
             <section className="flex flex-row justify-between items-center">
                 <CloseIcon setIsOpen={setIsOpen} isOpen={isOpen} />
             </section>
