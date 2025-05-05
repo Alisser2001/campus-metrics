09c3b5b5d83d5fbedb4cafe879f1a33b590fae74
Emma-Ok <emmanuel.bustamante@udea.edu.co>
2025-05-05 12:34:49 -0500

    Reemplazar contenido en la rama dev-test-design con nueva estructura

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,36 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "./components/theme-provider"
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-const inter = Inter({ subsets: ["latin"] })
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: "Sistema de Gestión de Archivos",
-  description: "Sistema de gestión de archivos para entidades institucionales",
-}
+  title: "Campus Metrics",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/udea.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
     </html>
-  )
+  );
 }
